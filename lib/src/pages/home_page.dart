569b8003529c5074155f1e129a9ffe7590b1e2c6
JosedeJesusMI <jchuchinislas74@gmail.com>
2021-04-14 19:48:57 -0500

    tarea

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final estiloText = new TextStyle(fontSize: 32);
+  int _contador = 0;
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material-Scalfold',
+      home: Scaffold(
+        appBar: AppBar(title: Text('hola grupo del 15701'), centerTitle: true),
+        body: Center(
+            //child: Text('Numero de Clicks',style: new TextStyle(fontSize: 30),),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('La cuentta va en',style: new TextStyle(fontSize: 25), ),
+            Text('$_contador',style: estiloText,
+            ),
+          ],
+        )),
+        floatingActionButton: _iconos(),
+      ),
+    );
+  }
+
+  Widget _iconos() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 10),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _suma,
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: _resta,
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero_outlined),
+          onPressed: _reset,
+        )
+      ],
+    );
+  }
+
+  void _suma() {
+    setState(() {
+      _contador++;
+    });
+  }
+
+  void _resta() {
+    setState(() {
+      _contador--;
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _contador = 0;
+    });
+  }
+}
